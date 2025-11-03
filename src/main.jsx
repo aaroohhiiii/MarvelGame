309992ba161bc91b70cd5f1ffddce514ae9b7b66
Aarohi <aarohi.g24@iiits.in>
2025-11-03 22:05:21 +0530

    Update UI features: Remove custom cursor, add TVA favicon, implement dynamic riddle shuffling

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import CustomCursor from './components/CustomCursor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CustomCursor />
     <App />
   </StrictMode>,
 )
